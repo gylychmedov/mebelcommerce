bef7c982897d6e7d983fb2faa6e78244072ec829
kickstertm <howesjen.01@gmail.com>
2021-07-28 19:06:12 +0300

    Update HomeAds.js

--- src/components/Ads/HomeAds.js
+++ src/components/Ads/HomeAds.js
@@ -2,19 +2,31 @@ import { SwiperSlide, Swiper } from "swiper/react";
 
 const HomeAds = () => {
   return (
-    <Swiper breakpoints={}>
-      <SwiperSlide>
+    <main className="grid grid-cols-12 gap-3">
+      {/* // <Swiper
+    //   spaceBetween={5}
+    //   slidesPerView={2}
+    //   breakpoints={{
+    //     640: {
+    //       slidesPerView: 4,
+    //     },
+    //   }}
+    // >
+    //   <SwiperSlide> */}
+      <div className="col-span-3">
         <img src="/banner/1.jpg" alt="Banner" />
-      </SwiperSlide>
-
-      <SwiperSlide>
+        {/* //   </SwiperSlide> */}
+      </div>
+      <div className="col-span-3">
         <img src="/banner/1.jpg" alt="Banner" />
-      </SwiperSlide>
-
-      <SwiperSlide>
+      </div>
+      <div className="col-span-3">
         <img src="/banner/1.jpg" alt="Banner" />
-      </SwiperSlide>
-    </Swiper>
+      </div>
+      <div className="col-span-3">
+        <img src="/banner/1.jpg" alt="Banner" />
+      </div>
+    </main>
   );
 };
 
