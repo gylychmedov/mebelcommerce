4c8d200c88e909bb40a4a1e6bb57439159e42fc9
kickstertm <78946365+kickstertm@users.noreply.github.com>
2021-08-20 22:53:21 +0300

    filter started

--- src/components/Header/Breadcrumb.js
+++ src/components/Header/Breadcrumb.js
@@ -4,68 +4,14 @@ import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
 
 const Breadcrumb = (props) => {
-  const { t } = useTranslation("common");
-  const { product = t("product"), category = t("categories") } = props;
-  const route = useRouter();
-  const splited = route.route.split("/");
-
-  const getLinkName = (name) => {
-    switch (name) {
-      case "": {
-        return <HiOutlineHome />;
-      }
-      case "Categories": {
-        return category;
-      }
-      case "sub": {
-        return ">";
-      }
-      case "[categories]": {
-        return product;
-      }
-      case "[subcategories]": {
-        return product;
-      }
-      case "Product": {
-        return t("product");
-      }
-      case "[product]": {
-        return product;
-      }
-    }
-  };
-
-  const getLink = (name) => {
-    switch (name) {
-      case "": {
-        return "/";
-      }
-      case "[categories]": {
-        return false;
-      }
-      case "[category]": {
-        return false;
-      }
-    }
-  };
   return (
     <main className="flex items-center px-5 py-2">
-      {splited.map((links) => {
-        return (
-          <>
-            {links != "" && (
-              <span className="mx-2 lg:mx-4 text-gray-400">/</span>
-            )}
-            {getLink(links) ? (
-              <Link href={getLink(links) || "/"}>
-                <a className="text-gray-500">{getLinkName(links)}</a>
-              </Link>
-            ) : (
-              <span className="text-gray-500">{getLinkName(links)}</span>
-            )}
-          </>
-        );
-      })}
+      <HiOutlineHome />
+      <span className="mx-2 lg:mx-4 text-gray-400">/</span>
+      <Link href="/">
+        <a className="text-gray-500"></a>
+      </Link>
+      <span className="text-gray-500"></span>
     </main>
   );
 };
--- src/pages/Categories/sub/[subcategories].js
+++ src/pages/Categories/sub/[subcategories].js
@@ -7,12 +7,239 @@ import { BsFillGridFill, BsFillGrid3X3GapFill } from "react-icons/bs";
 import { useRouter, withRouter } from "next/router";
 import { http } from "../../../components/API/http";
 import { ErrorApi } from "../../../components/Errors/Errors";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownToggle from "react-bootstrap/DropdownToggle";
+import DropdownMenu from "react-bootstrap/DropdownMenu";
 
 const subCategories = (props) => {
-  console.log(props);
   const [grid, setGrid] = useState(2);
   const { t } = useTranslation("common");
   const route = useRouter();
+  console.log(props);
+
+  const [load, setLoad] = useState(false);
+  const [products, setProducts] = useState(props.brands || []);
+  const [isDrawer, setIsDrawer] = useState({ visible: false });
+
+  // scroll states
+  const [scrollEnd, setScrollEnd] = useState(false);
+  const [page, setPage] = useState(props.page || 1);
+  const [ended, setEnded] = useState(false);
+  const [postReq, setPosReq] = useState(false);
+  let loadingMore = false;
+  let totalPage = props.pageCount || 1;
+
+  // // filter chekbox
+  const [filters, setFilters] = useState({
+    sizes: props.filters && props.filters.sizes ? props.filters.sizes : [],
+    colors: props.filters && props.filters.colors ? props.filters.colors : [],
+    materials:
+      props.filters && props.filters.materials ? props.filters.materials : [],
+    categories:
+      props.filters && props.filters.categories ? props.filters.categories : [],
+  });
+  const [filterProducts, setFilterProducts] = useState();
+  const [allFilters, setAllFilters] = useState({
+    sizes: [],
+    materials: [],
+    colors: [],
+    categories: [],
+  });
+
+  const newSize = filters.sizes.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, true);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  const handleScroll = () => {
+    let clientScreen = document.body.clientHeight + 800;
+    if (
+      document.body.scrollHeight - document.scrollingElement.scrollTop <=
+      clientScreen
+    ) {
+      setScrollEnd(true);
+    }
+  };
+
+  // sazlandy
+  useEffect(async () => {
+    if (
+      allFilters.sizes.length >= 1 ||
+      allFilters.materials.length >= 1 ||
+      allFilters.colors.length >= 1 ||
+      allFilters.categories.length >= 1
+    ) {
+      await http
+        .post(
+          `category/${props.slug}/products`,
+          {
+            materials: allFilters.materials,
+            sizes: allFilters.sizes,
+            categories: allFilters.categories,
+            colors: allFilters.colors,
+          },
+          {}
+        )
+        .then((res) => {
+          if (res.status === 200) {
+            totalPage = res.data.meta.last_page;
+            setFilterProducts(res.data.data.products);
+            setLoad(false);
+            setPage(1);
+            setFilters({
+              sizes: res.data.data.filters.sizes,
+              colors: res.data.data.filters.colors,
+              materials: res.data.data.filters.materials,
+              categories: res.data.data.filters.categories,
+            });
+          }
+        });
+    } else {
+      totalPage = props.pageCount || 1;
+      setFilters({
+        sizes: props.filters && props.filters.sizes ? props.filters.sizes : [],
+        colors:
+          props.filters && props.filters.colors ? props.filters.colors : [],
+        materials:
+          props.filters && props.filters.materials
+            ? props.filters.materials
+            : [],
+        categories:
+          props.filters && props.filters.categories
+            ? props.filters.categories
+            : [],
+      });
+      setFilterProducts();
+      setLoad(false);
+    }
+  }, [allFilters]);
+
+  const handleFilterSize = (event, id) => {
+    if (event.target.checked == true) {
+      let newSize = [...allFilters.sizes];
+      allFilters.sizes.includes(id) == false && newSize.push(id);
+      setAllFilters({ ...allFilters, sizes: [...newSize] });
+    }
+
+    if (event.target.checked == false) {
+      let newSize = [...allFilters.sizes];
+      let index = newSize.indexOf(id);
+      newSize.splice(index, 1);
+      setAllFilters({ ...allFilters, sizes: [...newSize] });
+    }
+    setLoad(!load);
+  };
+
+  const handleFilterMaterial = (event, id) => {
+    if (event.target.checked == true) {
+      let newMaterial = [...allFilters.materials];
+      allFilters.materials.includes(id) == false && newMaterial.push(id);
+      setAllFilters({ ...allFilters, materials: [...newMaterial] });
+    }
+
+    if (event.target.checked == false) {
+      let newMaterial = [...allFilters.materials];
+      let index = newMaterial.indexOf(id);
+      newMaterial.splice(index, 1);
+      setAllFilters({ ...allFilters, materials: [...newMaterial] });
+    }
+    setLoad(!load);
+  };
+
+  const handleFilterCategory = (event, id) => {
+    if (event.target.checked == true) {
+      let newCategories = [...allFilters.categories];
+      allFilters.categories.includes(id) == false && newCategories.push(id);
+      setAllFilters({ ...allFilters, categories: [...newCategories] });
+    }
+
+    if (event.target.checked == false) {
+      let newCategories = [...allFilters.categories];
+      let index = newCategories.indexOf(id);
+      newCategories.splice(index, 1);
+      setAllFilters({ ...allFilters, categories: [...newCategories] });
+    }
+    setLoad(!load);
+  };
+
+  const handleFilterColor = (event, id) => {
+    if (event.target.checked == true) {
+      let newColor = [...allFilters.colors];
+      newColor.push(parseInt(id));
+      setAllFilters({ ...allFilters, colors: [...newColor] });
+    }
+
+    if (event.target.checked == false) {
+      let newColor = [...allFilters.colors];
+      let index = newColor.indexOf(parseInt(id));
+      newColor.splice(index, 1);
+      setAllFilters({ ...allFilters, colors: [...newColor] });
+    }
+    setLoad(!load);
+  };
+
+  if (scrollEnd == true) {
+    loadingMore = true;
+    if (postReq == false && page >= totalPage) {
+      setPosReq(true);
+      setEnded(true);
+      setTimeout(() => {
+        setPosReq(false);
+        loadingMore = false;
+      }, 5000);
+    }
+    if (postReq == false && page < totalPage) {
+      setPosReq(true);
+      if (
+        allFilters.sizes.length >= 1 ||
+        allFilters.materials.length >= 1 ||
+        allFilters.colors.length >= 1 ||
+        allFilters.categories.length >= 1
+      ) {
+        http
+          .post(
+            `category/${props.slug}/products?page=${page + 1}`,
+            {
+              materials: allFilters.materials,
+              sizes: allFilters.sizes,
+              categories: allFilters.categories,
+              colors: allFilters.colors,
+            },
+            {}
+          )
+          .then((res) => {
+            if (res.status === 200) {
+              totalPage = res.data.meta.last_page;
+              setFilterProducts([...filterProducts, ...res.data.data.products]);
+              setLoad(false);
+              setPage(page + 1);
+            }
+            if (res.data.data.products.length == 0) {
+              setEnded(true);
+            }
+          });
+      } else {
+        http
+          .post(`category/${props.slug}/products?page=${page + 1}`)
+          .then((res) => {
+            setProducts([...products, ...res.data.data.products]);
+            setPage(page + 1);
+          });
+      }
+
+      setTimeout(() => {
+        setPosReq(false);
+        setScrollEnd(false);
+        loadingMore = false;
+      }, 5000);
+    }
+  }
 
   return (
     <View>
@@ -21,7 +248,7 @@ const subCategories = (props) => {
       ) : (
         <main className="flex flex-col w-full ">
           <Breadcrumb product={route.query.subcategories} />
-          <section className="w-full flex items-center rounded-md bg-gray-100 py-3 px-4 my-5">
+          <section className="w-full flex items-center rounded-md bg-gray-100 py-2 px-4 my-5">
             {/* grid start */}
             <div className="flex items-center">
               <div className="mr-2">{t("view")}:</div>
@@ -43,6 +270,101 @@ const subCategories = (props) => {
               </div>
             </div>
             {/* grid end */}
+
+            <aside className="w-full  flex space-x-2 justify-end">
+              {/* colors */}
+              <Dropdown>
+                <DropdownToggle className="bg-white px-3 py-2 rounded-lg focus:ring-2">
+                  Colors
+                </DropdownToggle>
+                <DropdownMenu className="z-10">
+                  <div className="bg-white my-1 px-3 py-2 shadow rounded-lg">
+                    {filters.colors.map((color, key) => {
+                      return (
+                        <div key={key}>
+                          <input
+                            type="checkbox"
+                            checked={allFilters.colors.includes(color.id)}
+                            onChange={(event) =>
+                              handleFilterColor(event, color.id)
+                            }
+                            checked={allFilters.colors.includes(color.id)}
+                            id={`color_` + color.id}
+                          />
+                          <label htmlFor={`color_` + color.id} className="ml-2">
+                            {route.locale == "en"
+                              ? color.name_en
+                              : color.name_fr}
+                          </label>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </DropdownMenu>
+              </Dropdown>
+              {/* sizes */}
+              <Dropdown>
+                <DropdownToggle className="bg-white px-3 py-2 rounded-lg focus:ring-2">
+                  Sizes
+                </DropdownToggle>
+                <DropdownMenu className="z-10">
+                  <div className="bg-white my-1 px-3 py-2 shadow rounded-lg">
+                    {filters.sizes.map((size, key) => {
+                      return (
+                        <div key={key}>
+                          <input
+                            type="checkbox"
+                            checked={allFilters.sizes.includes(size.id)}
+                            onChange={(event) =>
+                              handleFilterSize(event, size.id)
+                            }
+                            checked={allFilters.sizes.includes(size.id)}
+                            id={`size_` + size.id}
+                          />
+                          <label htmlFor={`size_` + size.id} className="ml-2">
+                            {route.locale == "en" ? size.name_en : size.name_fr}
+                          </label>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </DropdownMenu>
+              </Dropdown>
+
+              {/* material */}
+              <Dropdown>
+                <DropdownToggle className="bg-white px-3 py-2 rounded-lg focus:ring-2">
+                  Material
+                </DropdownToggle>
+                <DropdownMenu className="z-10">
+                  <div className="bg-white my-1 px-3 py-2 shadow rounded-lg">
+                    {filters.materials.map((material, key) => {
+                      return (
+                        <div key={key}>
+                          <input
+                            type="checkbox"
+                            checked={allFilters.materials.includes(material.id)}
+                            onChange={(event) =>
+                              handleFilterMaterial(event, material.id)
+                            }
+                            checked={allFilters.materials.includes(material.id)}
+                            id={`material_` + material.id}
+                          />
+                          <label
+                            htmlFor={`material_` + material.id}
+                            className="ml-2"
+                          >
+                            {route.locale == "en"
+                              ? material.name_en
+                              : material.name_fr}
+                          </label>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </DropdownMenu>
+              </Dropdown>
+            </aside>
           </section>
           <section
             className={`grid ${
@@ -62,8 +384,7 @@ const subCategories = (props) => {
 export const getServerSideProps = async ({ query }) => {
   try {
     const slug = query.subcategories;
-    const products = await http.post(`center/${slug}/products`);
-
+    const products = await http.post(`category/${slug}/products`);
     return {
       props: {
         pageCount: products.data.meta.last_page,
@@ -71,13 +392,13 @@ export const getServerSideProps = async ({ query }) => {
         products: products.data.data.products,
         filters: products.data.data.filters,
         slug: slug,
-        notFound: false,
+        error: false,
       },
     };
   } catch {
     return {
       props: {
-        notFound: true,
+        error: true,
       },
     };
   }
